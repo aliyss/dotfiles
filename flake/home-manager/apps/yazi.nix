a6f940dec874146f09d23bae8dd0c8d945a881e0
aliyss <silyas.bieri@lagomio.com>
2026-07-24 13:05:54 +0200

    yazi ssh

--- flake/home-manager/apps/yazi.nix
+++ flake/home-manager/apps/yazi.nix
@@ -1,16 +1,21 @@
-{config, pkgs, ...}: let
-  yazi-glow = pkgs.fetchFromGitHub {
-    owner = "Reledia";
-    repo = "glow.yazi";
-    rev = "536185a4e60ac0adc11d238881e78678fdf084ff";
-    hash = "sha256-Dc/zdxfzAUM5NX8PxzfljRbYvO9f9syuLO8yBr+R3qg=";
-  };
-in {
+{config, pkgs, ...}: {
   programs.yazi = {
     enable = true;
-    # plugins = {
-    #   "glow.yazi" = yazi-glow;
-    # };
+    plugins = {
+      sshfs = {
+        package = pkgs.yaziPlugins.sshfs;
+        setup = true;
+      };
+    };
+    keymap = {
+      mgr.prepend_keymap = [
+        {
+          on = [ "g" "s" ];
+          run = "plugin sshfs -- menu";
+          desc = "Open SSHFS options";
+        }
+      ];
+    };
     shellWrapperName = "y";
     enableZshIntegration = true;
   };
--- flake/home-manager/packages.nix
+++ flake/home-manager/packages.nix
@@ -14,6 +14,7 @@ let
     tridactyl-native
     chawan
     chromium
+    sshfs-fuse
     google-chrome
     btop
     yarn-berry
