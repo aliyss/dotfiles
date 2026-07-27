db755a1d9587f2449a1e9bb2f4feb2713ba7c3b1
aliyss <mangoworksbeta@gmail.com>
2026-07-27 10:26:40 +0200

    adjustments

--- flake/home-manager/packages/work.nix
+++ flake/home-manager/packages/work.nix
@@ -1,12 +1,11 @@
-{ pkgs, ... }:
-let
+{pkgs, ...}: let
   gdk = pkgs.google-cloud-sdk.withExtraComponents (with pkgs.google-cloud-sdk.components; [
     gke-gcloud-auth-plugin
   ]);
-in with pkgs; [
-  slack
-  anydesk
-  gdk
-  teams-for-linux
-  rustdesk
-]
+in
+  with pkgs; [
+    # slack
+    # anydesk
+    # gdk
+    # teams-for-linux
+  ]
