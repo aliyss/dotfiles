1d2589d4a7c0ede0e9a1557336443c1c5e2d75d1
aliyss <silyas.bieri@lagomio.com>
2026-08-13 11:41:47 +0200

    update

--- hypr/hyprland_source.lua
+++ hypr/hyprland_source.lua
@@ -1,12 +1,12 @@
 local mainMod = "SUPER"
 
 
--- hl.monitor({
---     output   = "DP-2",
---     mode     = "5120x1440",
---     position = "0x0",
---     scale    = 1,
--- })
+hl.monitor({
+    output   = "DP-2",
+    mode     = "5120x1440",
+    position = "0x0",
+    scale    = 1,
+})
 --
 -- hl.monitor({
 --     output   = "DP-1",
